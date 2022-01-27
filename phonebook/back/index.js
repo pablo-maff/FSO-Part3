22e4eb378409d633e92f156d2ef81aa3d3a98a70
pablo-maff <pablo.maff.076@gmail.com>
2022-01-28 00:32:27 +0100

    uibuild

---
+++ phonebook/back/index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+
+morgan.token('person', req => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
+
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>\n
+    <p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) res.json(person)
+  else res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+const generateRandomId = (min=1, max=9999) => {
+  //The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+app.post('/api/persons', (req, res) => {
+  const personsNames = persons.map(name => name.name.toLowerCase())
+  const errorHandler = message => {
+    return res.status(400).json({
+      error: message
+    })
+  }
+
+  const body = req.body
+  
+  const person = {
+    "id": generateRandomId(),
+    "name": body.name || errorHandler('Person must have a name.'),
+    "number": body.number || errorHandler('Person must have a phone number.'),
+  }
+  
+  if (personsNames.includes(person.name.toLowerCase())) {
+    return errorHandler(`${person.name} is already registered in the phonebook.`)
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
