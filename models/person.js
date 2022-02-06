98f0c0428252f5376df27239c941aca597e46038
pablo-maff <pablo.maff.076@gmail.com>
2022-02-06 14:13:49 +0100

    Pass everything to root

---
+++ models/person.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+console.log(`connecting to ${url}`);
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message);
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: [3, 'Name must be at least 3 characters long'],
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: [8, 'Number must be at least 8 numbers long'],
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{4,}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number`
+    },
+    required: [true, 'Phone number is required']
+  }
+})
+
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+module.exports = Person
